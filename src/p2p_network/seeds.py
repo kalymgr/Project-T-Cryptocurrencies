39ede48fbbc63186dd640e694eeaf74b5360125f
Μιχάλης Τσουγκράνης <infoerawebdesign@gmail.com>
2018-11-29 07:40:48 +0200

    - Finished with this story. Two seed nodes have been added. A full node, when started, can connect to those seeds to get a list of peer nodes (using the getAddr message)

--- src/p2p_network/seeds.py
+++ src/p2p_network/seeds.py
@@ -7,11 +7,12 @@ from twisted.internet import reactor
 from src.p2p_network.parameters import NodeTypes
 from src.p2p_network.tlc_network import TLCNode
 
-# initialize two seed nodes running on ports 8018 and 8019 and ask them for some node addresses
+# Initialize two seed nodes running on ports 8018 and 8019
 seedNode1 = TLCNode(reactor, nodeType=NodeTypes.SEED_NODE, port=8018)
 seedNode1.addNodePeersList(['127.0.0.1_8015', '127.0.0.1_8016'])  # set the list for the first seed
 seedNode2 = TLCNode(reactor, nodeType=NodeTypes.SEED_NODE, port=8019)
 seedNode2.addNodePeersList(['127.0.0.1_8017', '127.0.0.1_8018'])  # set the list for the second seed
+# Start the seed nodes
 seedNode1.startNode()
 seedNode2.startNode()
 
